3faf184226b78a752aa097ac9aeb45843cee5cb0
Wills-M <wmm38@drexel.edu>
2020-08-03 13:53:22 -0400

    Added a phase manager to handle input vs resolve phases.

---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseManager : MonoBehaviour
+{
+    [SerializeField]
+    private Player player;
+
+    private IEnumerator turnCoroutine;
+
+    private void Start()
+    {
+        turnCoroutine = TurnPhase();
+        StartCoroutine(turnCoroutine);
+    }
+
+    private IEnumerator TurnPhase()
+    {
+        while (player.waitingForInput)
+        {
+            yield return null;
+        }
+        ResolvePhase();
+    }
+
+    private void ResolvePhase()
+    {
+        player.Resolve();
+        player.waitingForInput = true;
+        turnCoroutine = TurnPhase();
+        StartCoroutine(turnCoroutine);
+        //TODO: Resolve all objects and player ghost
+    }
+}
