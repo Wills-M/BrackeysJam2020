765ca445ac47179faed398d4a04265f03bd614ae
Wills-M <wmm38@drexel.edu>
2020-08-02 22:59:43 -0400

    Basic player movement.

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+
+    protected void Move(Vector2 direction)
+    {
+        // Check for block in offset position
+        Vector2 offsetPosition = new Vector2(transform.position.x, transform.position.y) + direction;
+        Collider2D result = Physics2D.OverlapPoint(offsetPosition);
+
+        // If there was no collider check for block to hold actor up
+        if (!result)
+        {
+            // As long as spot below current offset is empty move it down
+            result = Physics2D.OverlapPoint(offsetPosition + Vector2.down);
+            while (!result)
+            {
+                offsetPosition += Vector2.down;
+                result = Physics2D.OverlapPoint(offsetPosition + Vector2.down);
+            }
+
+            // Move Actor
+            transform.position = new Vector3(offsetPosition.x, offsetPosition.y);
+        }
+    }
+
+}
