ff8518dffd1e7ea2cf838ea034a15fd865a35a03
Drew Graham <dmg9626@gmail.com>
2020-08-02 22:11:42 -0400

    testing checkpoint

---
+++ Assets/Scripts/Player/CheckTile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTile : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Check((Vector2)transform.position + Vector2.left);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Check((Vector2)transform.position + Vector2.right);
+        }
+    }
+
+    void Check(Vector2 position)
+    {
+        Collider2D col = Physics2D.OverlapPoint(position);
+        if(col)
+        {
+            Debug.LogFormat("Collided with {0} at position {1}", col.name, position);
+        }
+    }
+}
