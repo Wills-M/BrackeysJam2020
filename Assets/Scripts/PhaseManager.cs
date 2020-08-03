87eb6b5f18c2408410c88a075bf53a1ae04b824f
Drew Graham <dmg9626@gmail.com>
2020-08-03 18:38:29 -0400

    started writing logic to handle multiple actors in PhaseManager

--- Assets/Scripts/PhaseManager.cs
+++ Assets/Scripts/PhaseManager.cs
@@ -7,14 +7,34 @@ public class PhaseManager : MonoBehaviour
     [SerializeField]
     private Player player;
 
+    private List<Actor> actors;
+
     private IEnumerator turnCoroutine;
+
+    /// <summary>
+    /// Starting position for actors
+    /// </summary>
+    public static Vector2 start;
+
+    private void Awake()
+    {
+        // Store player starting position
+        start = player.transform.position;
+
+        // Initialize actors list with player
+        actors = new List<Actor>() { player };
+    }
 
     private void Start()
     {
+        // Initiate action/resolve phase loop
         turnCoroutine = TurnPhase();
         StartCoroutine(turnCoroutine);
     }
 
+    /// <summary>
+    /// Waits for player before resolving actions in ResolvePhase()
+    /// </summary>
     private IEnumerator TurnPhase()
     {
         while (player.waitingForInput)
@@ -24,12 +44,19 @@ public class PhaseManager : MonoBehaviour
         ResolvePhase();
     }
 
+    /// <summary>
+    /// Resolves each actor's actions before looping back to TurnPhase()
+    /// </summary>
     private void ResolvePhase()
     {
-        player.Resolve();
+        // Resolve each actor's action
+        foreach(Actor actor in actors)
+        {
+            actor.Resolve();
+        }
         player.waitingForInput = true;
+
         turnCoroutine = TurnPhase();
         StartCoroutine(turnCoroutine);
-        //TODO: Resolve all objects and player ghost
     }
 }
