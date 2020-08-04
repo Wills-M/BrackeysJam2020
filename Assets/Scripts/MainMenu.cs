1de4fd6790d8e5e0348572a6fea1a712d48f5861
Wills-M <wmm38@drexel.edu>
2020-08-04 15:00:23 -0400

    Added main menu script to handle option selection.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<Text> options;
+    [SerializeField]
+    private Color selectedColor;
+    [SerializeField]
+    private Color deselectedColor;
+
+    private int selectedOption;
+
+    private void Start()
+    {
+        SelectOption(0);
+    }
+
+    private void SelectOption(int optionNumber)
+    {
+        foreach(Text text in options)
+        {
+            text.color = deselectedColor;
+        }
+        options[optionNumber].color = selectedColor;
+    }
+
+}
