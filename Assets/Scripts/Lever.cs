8c5583fb2162c49bf3b03bd92f706265dcfd984f
Wills-M <wmm38@drexel.edu>
2020-08-04 22:21:08 -0400

    Added a lever prefab.

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+
+    [SerializeField]
+    private bool on;
+    [SerializeField]
+    private Sprite offSprite;
+    [SerializeField]
+    private Sprite onSprite;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        UpdateSprite();
+    }
+
+    private void UpdateSprite()
+    {
+        if (on)
+            spriteRenderer.sprite = onSprite;
+        else
+            spriteRenderer.sprite = offSprite;
+    }
+
+    public void Flip()
+    {
+        on = !on;
+        UpdateSprite();
+    }
+
+}
