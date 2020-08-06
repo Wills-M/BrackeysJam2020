6bc3118f740afa091bc568ed9528e69a2688e863
Drew Graham <dmg9626@gmail.com>
2020-08-06 18:24:53 -0400

    implemented WaitTask for turn skipping

---
+++ Assets/Scripts/Actions/WaitTask.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class WaitTask : Task
+{
+    private float delay;
+
+    public WaitTask(float delay = 0f)
+    {
+        this.delay = delay;
+    }
+
+    public override bool CanPerform()
+    {
+        return true;
+    }
+
+    public override IEnumerator Execute()
+    {
+        // Wait for delay before ending execution
+        IsExecuting = true;
+        yield return new WaitForSeconds(delay);
+        IsExecuting = false;
+    }
+}
