410680e773142ba4b82f919506f2b53dabf46aeb
Wills-M <wmm38@drexel.edu>
2020-08-06 22:12:11 -0400

    Added static effect and screen distortion to rewind.

--- Assets/Scripts/Utilities/PostProcessingManager.cs
+++ Assets/Scripts/Utilities/PostProcessingManager.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
 
 public class PostProcessingManager : Singleton<PostProcessingManager>
 {
     
     private VolumeProfile profile;
 
+    [SerializeField]
+    private Image screenImage;
 
     [SerializeField]
     private float caIntensity;
     private ChromaticAberration chromaticAberration;
+
+    [SerializeField]
+    private float paniniIntensity;
+    private PaniniProjection paniniProjection;
+    private float startPanini;
 
     [SerializeField]
     private float inOutTime;
@@ -23,15 +31,21 @@ public class PostProcessingManager : Singleton<PostProcessingManager>
         profile = volume.profile;
 
         profile.TryGet(out chromaticAberration);
+        profile.TryGet(out paniniProjection);
+
+        startPanini = paniniProjection.distance.value;
     }
 
     private IEnumerator StartCoroutine()
     {
+        screenImage.enabled = true;
         for (float t = 0; t < inOutTime; t += Time.deltaTime)
         {
+            paniniProjection.distance.Override((t / inOutTime) * (paniniIntensity - startPanini) + startPanini);
             chromaticAberration.intensity.Override((t / inOutTime) * caIntensity);
             yield return null;
         }
+        paniniProjection.distance.Override(paniniIntensity);
         chromaticAberration.intensity.Override(caIntensity);
     }
 
@@ -40,9 +54,13 @@ public class PostProcessingManager : Singleton<PostProcessingManager>
 
         for (float t = 0; t < inOutTime; t += Time.deltaTime)
         {
-            chromaticAberration.intensity.Override((1f - (t / inOutTime)) * caIntensity);
+            paniniProjection.distance.Override((1f - (t / inOutTime)) * (paniniIntensity - startPanini) + startPanini);
+            //chromaticAberration.intensity.Override((1f - (t / inOutTime)) * caIntensity);
             yield return null;
         }
+
+        screenImage.enabled = false;
+        paniniProjection.distance.Override(startPanini);
         chromaticAberration.intensity.Override(0f);
     }
 
