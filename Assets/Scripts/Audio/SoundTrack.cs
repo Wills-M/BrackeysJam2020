47695e3b237f9526cb48f4cd452d2239d2097da8
Drew Graham <dmg9626@gmail.com>
2020-08-08 01:03:03 -0400

    imported my SoundController code to handle audio; playing music from main menu thru to levels using it

---
+++ Assets/Scripts/Audio/SoundTrack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+[CreateAssetMenu(fileName = "SoundTrack")]
+public class SoundTrack : ScriptableObject
+{   
+
+    public SoundType type;
+    public AudioClip clip;
+
+    [Range(0, 1)]
+    public float volume = 1;
+    public bool loop = false;
+}
+
+public enum SoundType
+{
+    NONE = 0,
+
+    // Looping music
+    BG_MUSIC = 001,
+
+    // Sound effects
+    PLAYER_MOVE = 101
+
+}
